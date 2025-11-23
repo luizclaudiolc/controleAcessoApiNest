70ca13bb3609c791343b8badb6fb74e26bdd8484
Luiz Claudio <luiz.claudiolc@outlook.com>
2025-11-23 14:57:09 -0300

    feat: integrate Prisma ORM and add User model with migration

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,15 @@
+import { Injectable, type OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    super({
+      log: ['query', 'warn'],
+    });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+  }
+}
